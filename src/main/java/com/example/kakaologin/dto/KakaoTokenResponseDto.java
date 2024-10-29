6980ddae6b18087139901ec55cdc116bd185a270
kdsss123 <ehdtjd3321@naver.com>
2024-10-29 16:14:53 +0900

    카카오로그인 구현

---
+++ src/main/java/com/example/kakaologin/dto/KakaoTokenResponseDto.java
@@ -0,0 +1,34 @@
+package com.example.kakaologin.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor // 역직렬화를 위한 기본 생성자
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class KakaoTokenResponseDto {
+
+    @JsonProperty("token_type")
+    public String tokenType;
+
+    @JsonProperty("access_token")
+    public String accessToken;
+
+    @JsonProperty("id_token")
+    public String idToken;
+
+    @JsonProperty("expires_int")
+    public String expiresIn;
+
+    @JsonProperty("refresh_token")
+    public String refreshToken;
+
+    @JsonProperty("refresh_token_expires_in")
+    public String refreshTokenExpiresIn;
+
+    @JsonProperty("scope")
+    public String scope;
+
+}
